36119475487e19e2e1755d92ef6c3ee3ab14c566
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-10-29 10:45:10 -0600

    Create HoD.meta.js

---
+++ HoD.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         HoD Tools
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Faction Tools For House of Dragonborn
+// @author       BjornOdinsson89
+// @match        https://www.torn.com/*
+// @match        https://www2.torn.com/*
+// @updateURL    https://github.com/BjornOdinsson89/HoD/raw/main/HoD.user.js
+// @downloadURL  https://github.com/BjornOdinsson89/HoD/raw/main/HoD.user.js
+// @grant        GM_addStyle
+// @grant        GM_notification
+// @grant        GM_xmlhttpRequest
+// @connect      github.com
+// @connect      api.torn.com
+// @connect      worldtimeapi.org
+// ==/UserScript==
